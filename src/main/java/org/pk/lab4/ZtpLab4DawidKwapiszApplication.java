ff38991035c3fa00480640f9661414624637ed26
dkwapisz <dkwapisz99@gmail.com>
2023-12-19 16:32:39 +0100

    Initial commit

---
+++ src/main/java/org/pk/lab4/ZtpLab4DawidKwapiszApplication.java
@@ -0,0 +1,13 @@
+package org.pk.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtpLab4DawidKwapiszApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtpLab4DawidKwapiszApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pk/lab4/ZtpLab4DawidKwapiszApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pk.lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpLab4DawidKwapiszApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
